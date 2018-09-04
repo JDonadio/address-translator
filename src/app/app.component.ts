3b807802548ffcd2411f1a405a4dd31d6b524480
Gabriel Bazán <gabrielbazan7@hotmail.com>
2018-09-04 12:07:32 -0300

    Improve page UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,13 @@ export class AppComponent {
   public copayAddr: string;
   public legacyAddr: string;
   public cashAddrUpperCase: string;
+  public copiedFormat: string;
+  public showCopyAlert: boolean;
 
   constructor(
     private addressTranslatorService: AddressTranslatorService
   ) {
+    this.showCopyAlert = false;
   }
 
   public translateAddress(addr: string): void {
@@ -24,5 +27,20 @@ export class AppComponent {
     this.copayAddr = this.addressTranslatorService.translateCopayAddress(this.legacyAddr);
     this.cashAddr = this.addressTranslatorService.translateCashAddress(this.copayAddr);
     this.cashAddrUpperCase = this.cashAddr.toUpperCase();
+  }
+
+  public copyToClipboard(addr: string, format: string): void {
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    textarea.value = addr;
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    this.showCopyAlert = true;
+    this.copiedFormat = format;
+  }
+
+  public close(): void {
+    this.showCopyAlert = false;
   }
 }
